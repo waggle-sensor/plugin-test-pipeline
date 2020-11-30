a0623807849be346087e69be8595d54d89c9a4c9
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-11-30 10:07:10 -0600

    first commit

---
+++ plugin.py
@@ -0,0 +1,23 @@
+import waggle.plugin as plugin
+from random import random
+from time import sleep
+
+plugin.init()
+
+while True:
+    sleep(10)
+    print('uploading data', flush=True)
+    plugin.upload(b'my binary data')
+
+    print('uploading file', flush=True)
+    with open('mydata.txt', 'w') as f:
+        f.write('this is a large data file to be sent to beehive')
+    plugin.upload_file('mydata.txt', labels={
+        'label1': 'value1',
+        'label2': 'value2',
+        'label3': 'value3',
+    })
+
+    value = 25.0 + 5*random()
+    print('publishing', value, flush=True)
+    plugin.publish('env.temperature', value)
