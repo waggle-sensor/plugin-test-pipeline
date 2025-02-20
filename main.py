86fa28ee149e9cacf749decc32caa289b6714abf
Sean Shahkarami <sean.shahkarami@gmail.com>
2025-02-20 12:24:08 -0600

    update pywaggle version

--- main.py
+++ main.py
@@ -11,14 +11,16 @@ def main():
     parser.add_argument("--rate", default=300.0, type=float, help="sampling rate")
     args = parser.parse_args()
 
-    logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO,
-                        format="%(asctime)s %(message)s",
-                        datefmt="%Y/%m/%d %H:%M:%S")
+    logging.basicConfig(
+        level=logging.DEBUG if args.debug else logging.INFO,
+        format="%(asctime)s %(message)s",
+        datefmt="%Y/%m/%d %H:%M:%S",
+    )
 
     with Plugin() as plugin:
         while True:
             logging.info("publishing random measurement")
-            plugin.publish("test", 25.0 + 5*random())
+            plugin.publish("test", 25.0 + 5 * random())
 
             logging.info("uploading test image file")
             plugin.upload_file("test.jpg", keep=True)
